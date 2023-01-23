c987ae4154828dd6be8d90e385ed2c9a92d9dcf7
Michael Browne <brownemm@bc.edu>
2023-01-22 22:36:25 -0500

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Michael Browne on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
