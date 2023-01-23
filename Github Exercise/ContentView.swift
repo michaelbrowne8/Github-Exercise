14f34236992215f28df9604d8e500ddb53d74d2d
Michael Browne <brownemm@bc.edu>
2023-01-22 22:47:16 -0500

    Version 2.0 - Green

--- Github Exercise/ContentView.swift
+++ Github Exercise/ContentView.swift
@@ -13,12 +13,12 @@ struct ContentView: View {
             Image(systemName: "1.circle")
                 .resizable()
                 .scaledToFit()
-            Text("Version 1.0!")
+            Text("Version 2.0!")
                 .font(.largeTitle)
                 .fontWeight(.black)
         }
         .padding()
-        .foregroundColor(.red)
+        .foregroundColor(.green)
     }
 }
 
